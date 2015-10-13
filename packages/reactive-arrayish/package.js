2f464ae5fbd5b57c10646824554a94c7826f44b6
Jeremy Chen <convexset@gmail.com>
2015-10-14 02:28:32 +0800

    Properly scoped tests to client.

--- packages/reactive-arrayish/package.js
+++ packages/reactive-arrayish/package.js
@@ -14,7 +14,7 @@ Package.onUse(function(api) {
 });
 
 Package.onTest(function(api) {
-	api.use('tinytest');
+	api.use(['tinytest'], 'client');
 	api.use(['ecmascript', 'mongo', 'tracker', 'underscore', 'convexset:reactive-arrayish'], 'client');
 	api.addFiles(['tests.js'], 'client');
 });
